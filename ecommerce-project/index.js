849f42570283b9e9b53df1e2104779d3b68b0fa9
Axel Espinosa <axeldavid45@gmail.com>
2020-08-31 11:43:37 -0500

    Added static files to the project

--- ecommerce-project/index.js
+++ ecommerce-project/index.js
@@ -4,6 +4,10 @@ const { join } = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 const productsRoutes = require('./routes/products')
+
+// Static files
+app.use('/static', express.static(join(__dirname, 'public')))
+
 // Define the template engine
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'pug')
