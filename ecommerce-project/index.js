cd0f04987df06b7a77ce16a13db3cc9f140e6f13
Axel Espinosa <axeldavid45@gmail.com>
2020-08-31 10:55:25 -0500

    Added pug as a template Engine

---
+++ ecommerce-project/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const express = require('express')
+const { join } = require('path')
+const app = express()
+const PORT = process.env.PORT || 3000
+const productsRoutes = require('./routes/products')
+// Define the template engine
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// Routes
+app.use('/products', productsRoutes)
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
